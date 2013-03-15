a6136ee40575fa9650bcd022904df151051e9b76
Tong <tongyi121@gmail.com>
2013-03-15 10:46:17 +0800

    update version to 0.5.2

--- js/bootstrap-ajax.js
+++ js/bootstrap-ajax.js
@@ -1,5 +1,5 @@
 /* ====================================================================
- * bootstrap-ajax.js v0.5.1
+ * bootstrap-ajax.js v0.5.2
  * ====================================================================
  *rewrite by tongyi121,
  *add something remove something in my idea.
@@ -38,20 +38,19 @@ function executeFunctionByName(functionName, context /*, args */) {
         bootAjax(params);
     };
     Ajax.prototype.submit = function (e) {
-        var $this = $(this),
-            params = bindParams($this);
+        e.preventDefault();
+        var $this = $(this)
+        if(!e.data.duplicate)
+            $this.find("input[type=submit],button[type=submit]").attr("disabled", "disabled");
+        var params = bindParams($this);
         params.url = $this.attr('action');
         params.data = $this.serialize();
         if (!params.method) {
             params.method = 'POST'
         }
-        e.preventDefault();
-        if (!$this.find("input,select,textarea").not("[type=submit]").jqBootstrapValidation("hasErrors")) {
-            $this.find("input[type=submit],button[type=submit]").attr("disabled", "disabled");
-            bootAjax(params);
-        }
-
+        bootAjax(params);
     };
+
 
     function bindParams($this) {
         return {
@@ -67,7 +66,7 @@ function executeFunctionByName(functionName, context /*, args */) {
         }
     }
 
-    var spinOpts = {radius: 20,lines: 10, length: 5, width: 10, color: '#000', trail: 30,zIndex:2};
+    var spinOpts = {radius: 25, length: 0, width: 10, color: '#C40000', trail: 40,zIndex: 2};
 
     function spin(selector, flag) {
         var opts = (flag == true ? spinOpts : false);
@@ -121,15 +120,15 @@ function executeFunctionByName(functionName, context /*, args */) {
                     if (dataType == 'json') {
                         if(data.html){
                             if (append) {
-                                $el.append(data.html).bootAjax();
+                                $el.append(data.html).bootAjaxLink().bootValidation();
                             } else
-                                $el.html(data.html).bootAjax();
+                                $el.html(data.html).bootAjaxLink().bootValidation();
                         }
                     } else {
                         if (append) {
-                            $el.append(data).bootAjax();
+                            $el.append(data).bootAjaxLink().bootValidation();
                         } else
-                            $el.html(data).bootAjax();
+                            $el.html(data).bootAjaxLink().bootValidation();
                     }
                 }
                 if (dataType == 'json') {
@@ -193,24 +192,45 @@ function executeFunctionByName(functionName, context /*, args */) {
     }
 
     $.fn.bootAjax = function (params) {
-        if (params) {
-            params.el = $(this);
-            bootAjax(params);
-        } else {
-            return this.each(function () {
-                var $this = $(this);
-                $this.find('a.ajax').bind('click', Ajax.prototype.click);
-                $this.find('form.ajax').bind('submit', Ajax.prototype.submit);
-                //以下依赖于datepicker与jqBootstrapValidation
-                $this.find('.datepicker').datepicker({autoclose:true});
-                $this.find("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
-            });
-        }
-
+        params.el = $(this);
+        bootAjax(params);
     };
+    $.fn.bootAjaxLink = function(){
+        var $this = $(this);
+        $this.find('a.ajax').bind('click', Ajax.prototype.click);
+        return $this;
+    };
+    $.fn.bootAjaxForm = function(duplicate){
+        var $this = $(this);
+        if($(this).is("form"))
+            $this.bind('submit',{duplicate:duplicate}, Ajax.prototype.submit);
+        else
+            $this.find('form.ajax').bind('submit',{duplicate:duplicate}, Ajax.prototype.submit);
+        return $this;
+    }
+
+    $.fn.bootAjaxSubmit = function(params){
+        var $this = $(this);
+        if($this.is("form")) {
+            if(!params){
+                params = {}
+            }
+            params=$.extend(bindParams($this),params);
+
+            if(!params.url){
+                params.url = $this.attr('action');
+                params.data = $this.serialize();
+            }
+            params.data = $this.serialize();
+            if (!params.method) {
+                params.method = 'POST'
+            }
+            bootAjax(params);
+        }
+    }
 
     $(function () {
-        $("body").bootAjax();
+        $("body").bootAjaxLink();
     })
 
 }(window.jQuery);
